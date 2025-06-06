29a39190eebacf0b58d67c047cc6926768004281
Sarthak Navalakha <sarthaknavalakha@gmail.com>
2025-06-06 14:13:01 +0530

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -43,7 +43,9 @@ const Index = () => {
     
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:5678/webhook/8186e3fd-4088-4dbd-83a9-249867c64014', {
+      //test - http://localhost:5678/webhook-test/8186e3fd-4088-4dbd-83a9-249867c64014
+      //prod - http://localhost:5678/webhook/8186e3fd-4088-4dbd-83a9-249867c64014
+      const response = await fetch('http://localhost:5678/webhook-test/8186e3fd-4088-4dbd-83a9-249867c64014', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
