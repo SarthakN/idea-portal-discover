50cff045fcc4ebb8f1e79d55710bd1c4e07a93d6
Sarthak Navalakha <sarthaknavalakha@gmail.com>
2025-06-15 17:50:51 +0530

    BOTTT

--- src/components/IdeaFlowAnimation.tsx
+++ src/components/IdeaFlowAnimation.tsx
@@ -236,7 +236,7 @@ const IdeaFlowAnimation: React.FC<IdeaFlowAnimationProps> = ({ activeFeature }) 
           </div>
 
           {/* AI Bots - More spread out positions */}
-          <div className="absolute" style={{ left: '20%', top: '25%', zIndex: 2 }}>
+          <div className="absolute" style={{ left: '30%', top: '25%', zIndex: 2 }}>
             <div className="flex flex-col items-center space-y-2">
               <div className="relative">
                 <Bot className="h-12 w-12 text-purple-500 animate-bounce" style={{ animationDelay: '0s' }} />
@@ -246,7 +246,7 @@ const IdeaFlowAnimation: React.FC<IdeaFlowAnimationProps> = ({ activeFeature }) 
             </div>
           </div>
           
-          <div className="absolute" style={{ left: '80%', top: '25%', zIndex: 2 }}>
+          <div className="absolute" style={{ left: '70%', top: '25%', zIndex: 2 }}>
             <div className="flex flex-col items-center space-y-2">
               <div className="relative">
                 <Bot className="h-12 w-12 text-purple-500 animate-bounce" style={{ animationDelay: '0.5s' }} />
@@ -256,7 +256,7 @@ const IdeaFlowAnimation: React.FC<IdeaFlowAnimationProps> = ({ activeFeature }) 
             </div>
           </div>
           
-          <div className="absolute" style={{ left: '20%', top: '75%', zIndex: 2 }}>
+          <div className="absolute" style={{ left: '30%', top: '75%', zIndex: 2 }}>
             <div className="flex flex-col items-center space-y-2">
               <div className="relative">
                 <Bot className="h-12 w-12 text-purple-500 animate-bounce" style={{ animationDelay: '1s' }} />
@@ -266,7 +266,7 @@ const IdeaFlowAnimation: React.FC<IdeaFlowAnimationProps> = ({ activeFeature }) 
             </div>
           </div>
           
-          <div className="absolute" style={{ left: '80%', top: '75%', zIndex: 2 }}>
+          <div className="absolute" style={{ left: '70%', top: '75%', zIndex: 2 }}>
             <div className="flex flex-col items-center space-y-2">
               <div className="relative">
                 <Bot className="h-12 w-12 text-purple-500 animate-bounce" style={{ animationDelay: '1.5s' }} />
